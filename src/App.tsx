96aade2a98de24b03052f8dde08a71314dc5a69b
shahin325-admin <shahin.safari1369@gmail.com>
2026-06-03 17:40:35 +0300

    added tailwind grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
-
 function App() {
   return (
-    <>
-      <Button colorScheme="blue">Button</Button>
-    </>
+    <div className="grid h-screen grid-rows-[60px_1fr]">
+      {/* Navbar */}
+      <header className="border-b p-4">Navigation</header>
+
+      {/* Content area */}
+      <div className="grid grid-cols-[250px_1fr]">
+        {/* Sidebar */}
+        <aside className="hidden md:block border-r p-4">Sidebar</aside>
+
+        {/* Main content */}
+        <main className="p-4">Main Content</main>
+      </div>
+    </div>
   );
 }
-
 export default App;
