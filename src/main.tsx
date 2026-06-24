ceec48910f89c6b69b7a316510a106a2568fe5fd
shahin325-admin <shahin.safari1369@gmail.com>
2026-06-24 11:53:22 +0300

    added react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,30 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "./index.css";
 import App from "./App.tsx";
 
+// global query settings
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      staleTime: 10 * 1000, //10 sec, after 10 sec causes refresh
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: true,
+      refetchOnMount: true,
+    },
+  },
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+
+      <ReactQueryDevtools></ReactQueryDevtools>
+    </QueryClientProvider>
   </StrictMode>,
 );
